b991b3796a02370fc32a5db749044882170181c5
hrishikeshmohan23 <57557526+hrishikeshmohan23@users.noreply.github.com>
2023-10-22 20:35:41 +0530

    Revert "Added Solution to LinearSearch.cpp"
    This reverts commit 74039c50f030f6bab42c2fb37a186673cf051b86.

--- C++/reverseInteger.cpp
+++ C++/reverseInteger.cpp
@@ -1,46 +1,36 @@
-class Solution {
-public:
-    int reverse(int x) {
-        int flag=0;
-        if(x==INT_MIN){
-            return 0;
+/*Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+
+Example 1:
+
+Input: x = 123
+Output: 321
+
+
+Example 2:
+
+Input: x = -123
+Output: -321
+
+
+Example 3:
+
+Input: x = 120
+Output: 21*/
+
+
+#include<bits/stdc++.h>
+using namespace std;
+int reverse(int x) {
+       int reverse = 0;
+        while(x != 0) {
+            int digit = x % 10;
+            if(reverse > INT_MAX / 10 || reverse == INT_MAX / 10 && digit > 7) return 0;
+              if(reverse < INT_MIN / 10 || reverse == INT_MIN / 10 && digit < -8) return 0;
+            reverse = (reverse * 10) + digit;
+            x = x / 10;
         }
-        if(x<0){
-            flag=1;
-            x=-x;
-        }
-        vector <int> v;
-        while(x){
-            int a=x%10;
-            v.push_back(a);
-            x/=10;
-        }
-        int flag2=0;
-        int ans=0,m=10;
-        long check=ans;
-        for(int i=0;i<v.size();i++){
-            if(v[i]==0 && flag2==0){
-                continue;
-            }
-            else if(v[i]==0 && flag2==1){
-                if(check*m>INT_MAX){
-                    return 0;
-                }
-                ans=ans*m + v[i];
-                check=ans;
-            }
-            else{
-                if(check*m>INT_MAX){
-                    return 0;
-                }
-                ans=ans*m+v[i];
-                check=ans;
-                flag2=1;
-            }
-        }
-        if(flag==1){
-            ans=-ans;
-        }
-        return ans;
+        return reverse;
     }
-};
