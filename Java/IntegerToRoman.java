ee1e013cafbb6d6231a0f847e9c7f9b4d3f9ebae
Yasar Labib <125937329+yasarlabib@users.noreply.github.com>
2023-10-23 23:52:37 -0500

    added IntegerToRoman (#111)

---
+++ Java/IntegerToRoman.java
@@ -0,0 +1,15 @@
+public class IntegerToRoman {
+    public static String intToRoman(int num) {
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        String[] symbols = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        StringBuilder result = new StringBuilder();
+
+        for (int i = 0; i < values.length; i++) {
+            while (num >= values[i]) {
+                result.append(symbols[i]);
+                num -= values[i];
+            }
+        }
+
+        return result.toString();
+    }
