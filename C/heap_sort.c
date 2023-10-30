29bfe9ffa03ae8bd1ebfd33b6ecac6a9417726be
Ankit Poudel <108212023+ankman007@users.noreply.github.com>
2023-10-30 12:54:46 +0545

    Implemented Heap Sort In C

---
+++ C/heap_sort.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+void heapify(int arr[], int n, int i) {
+    int largest = i;  
+    int left = 2 * i + 1;  
+    int right = 2 * i + 2;  
+
+    // If left child is larger than root
+    if (left < n && arr[left] > arr[largest])
+        largest = left;
+
+    // If right child is larger than largest so far
+    if (right < n && arr[right] > arr[largest])
+        largest = right;
+
+    // If largest is not root
+    if (largest != i) {
+        int temp = arr[i];
+        arr[i] = arr[largest];
+        arr[largest] = temp;
+
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n) {
+    for (int i = n / 2 - 1; i >= 0; i--)
+        heapify(arr, n, i);
+
+    for (int i = n - 1; i > 0; i--) {
+        int temp = arr[0];
+        arr[0] = arr[i];
+        arr[i] = temp;
+
+        heapify(arr, i, 0);
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; ++i)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Original array:\n");
+    printArray(arr, n);
+
+    heapSort(arr, n);
+
+    printf("Sorted array using Heap Sort:\n");
+    printArray(arr, n);
+    return 0;
+}
